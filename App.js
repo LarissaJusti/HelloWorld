58469a1babc79666df3f83eb785fdb7f6c20cdb2
Larissa Justi <larissacalcanhojusti@gmail.com>
2023-04-05 22:35:57 -0300

    eigatinha

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import reactDom from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Pra falar de amor</Text>
+      <Text style={styles.titulotitulo}>Ei Gatinha!</Text>
+      <text style={styles.subtitulo}>Te conheço desde quando ce engatinha</text>
+      <text style={styles.subsubtitulo}>Queria tanto que ce fosse minha, nossos ideais cabem numa caixinha</text>
+    
     </View>
   );
 }
@@ -17,4 +21,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    borderRadius:5,
+    backgroundColor: "#47fe2c",
+    padding:10,
+    fontSize:50,
+  },
+
+  subtitulo: {
+    fonteSize:52,
+    backgroundColor: "#68d114",
+    marginTop:15,
+    borderRadius:5,
+  },
+
+  titulotitulo: {
+    fonteSize:85,
+    backgroundColor: "#68d114",
+    marginTop:15,
+    borderRadius:5,
+  }, 
+  subsubtitulo: {
+    fonteSize:85,
+    backgroundColor: "#68d114",
+    marginTop:15,
+    borderRadius:5,
+
+  }
+
+  
 });
