c1ef36a5276b85663d9cfa65089f76dfc225eb4b
Larissa Justi <larissacalcanhojusti@gmail.com>
2023-04-12 22:40:37 -0300

    Explicacao

--- App.js
+++ App.js
@@ -1,53 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import reactDom from 'react-dom';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Importa as páginas da aplicação
+import Home from './src/pages/Home';
+import Sobre from './src/pages/Sobre';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Pra falar de amor</Text>
-      <Text style={styles.titulotitulo}>Ei Gatinha!</Text>
-      <text style={styles.subtitulo}>Te conheço desde quando ce engatinha</text>
-      <text style={styles.subsubtitulo}>Queria tanto que ce fosse minha, nossos ideais cabem numa caixinha</text>
-    
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name= "Home" component={Home}/>
+        <Stack.Screen name= "Sobre" component={Sobre} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  titulo: {
-    borderRadius:5,
-    backgroundColor: "#47fe2c",
-    padding:10,
-    fontSize:50,
-  },
-
-  subtitulo: {
-    fonteSize:52,
-    backgroundColor: "#68d114",
-    marginTop:15,
-    borderRadius:5,
-  },
-
-  titulotitulo: {
-    fonteSize:85,
-    backgroundColor: "#68d114",
-    marginTop:15,
-    borderRadius:5,
-  }, 
-  subsubtitulo: {
-    fonteSize:85,
-    backgroundColor: "#68d114",
-    marginTop:15,
-    borderRadius:5,
-
-  }
-
-  
-});
